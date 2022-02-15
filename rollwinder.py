bdf658f4c990853a045667d5ea7988fb88509515
CatMe0w <CatMe0w@live.cn>
2022-02-16 01:12:23 +0800

    Rework rollwinder

--- rollwinder.py
+++ rollwinder.py
@@ -1,5 +1,7 @@
 import requests
 import logging
+import hashlib
+import json
 import time
 
 TIEBA_NAME = ''
@@ -8,50 +10,36 @@ TBS = ''
 BDUSS = ''
 
 
-def rollback(tid, pid, is_comment):
-    cookies = {
-        'BDUSS': BDUSS,
-    }
+def add_sign(data):
+    _ = ''
+    for (k, v) in sorted(data.items()):
+        _ += (k + '=' + v)
+    _ += 'tiebaclient!!!'
 
-    headers = {
-        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
-        'DNT': '1',
-        'sec-ch-ua-mobile': '?0',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Referer': 'https://tieba.baidu.com/p/' + tid,
-        'X-Requested-With': 'XMLHttpRequest',
-        'sec-ch-ua-platform': '"Windows"',
-    }
+    sign = hashlib.md5(_.encode('utf-8')).hexdigest().upper()
+    data.update({'sign': str(sign)})
+    return data
 
+
+def delete_thread(tid):
     data = {
-        'commit_fr': 'pb',
-        'ie': 'utf-8',
-        # 'tbs': 'ade683d96421bb181639555518',
-        'tbs': TBS,
-        'kw': TIEBA_NAME,
+        'BDUSS': BDUSS,
         'fid': TIEBA_FID,
-        'tid': tid,
-        'is_vipdel': '0',
-        'reason': '6',
+        'is_frs_mask': 0,
+        'tbs': TBS,
+        'z': tid
     }
-
-    if pid != 0:
-        data.update({'pid': pid})
-        if is_comment == '1':
-            data.update({'id_finf': '1'})
-        else:
-            data.update({'is_finf': 'false'})  # 百！！！！！！！度！！！！！！！！！！！
-
+    signed_data = add_sign(data)
     while True:
         try:
-            logging.info('Rollbacking thread {}, post {}'.format(tid, pid if pid else None))
-            response = requests.post('https://tieba.baidu.com/f/commit/post/delete',
-                                     headers=headers, data=data, cookies=cookies)
+            logging.info('Rollbacking thread {}'.format(tid))
+            response = requests.post('http://c.tieba.baidu.com/c/c/bawu/delthread', data=signed_data)
             if response.status_code != 200:
                 raise ValueError
-            logging.info('Response: {}'.format(response.text))
+            content = json.loads(response.content)
+            if int(content['error_code']):
+                logging.error('Rollback failed.')
+            logging.info('Response: {}'.format(content))
         except requests.exceptions.Timeout:
             print('Remote is not responding, sleep for 30s.')
             time.sleep(30)
@@ -62,6 +50,44 @@ def rollback(tid, pid, is_comment):
             continue
         else:
             break
+
+
+def delete_post(tid, pid):
+    data = {
+        'BDUSS': BDUSS,
+        'fid': TIEBA_FID,
+        'pid': pid,
+        'tbs': TBS,
+        'z': tid
+    }
+    signed_data = add_sign(data)
+    while True:
+        try:
+            logging.info('Rollbacking thread {}, post {}'.format(tid, pid))
+            response = requests.post('http://c.tieba.baidu.com/c/c/bawu/delpost', data=signed_data)
+            if response.status_code != 200:
+                raise ValueError
+            content = json.loads(response.content)
+            if int(content['error_code']):
+                logging.error('Rollback failed.')
+            logging.info('Response: {}'.format(content))
+        except requests.exceptions.Timeout:
+            print('Remote is not responding, sleep for 30s.')
+            time.sleep(30)
+            continue
+        except ValueError:
+            print('Rate limit exceeded, sleep for 30s.')
+            time.sleep(30)
+            continue
+        else:
+            break
+
+
+def rollback(tid, pid):
+    if pid == 0:
+        delete_thread(tid)
+    else:
+        delete_post(tid, pid)
 
 
 def main():
@@ -77,8 +103,8 @@ def main():
         thread_list = f.readlines()
 
     for thread in reversed(thread_list):
-        tid, pid, is_comment = thread.strip().split(' ')
-        rollback(tid, pid, is_comment)
+        tid, pid, _ = thread.strip().split(' ')
+        rollback(int(tid), int(pid))
 
     logging.info('All done! Have fun!')
 
